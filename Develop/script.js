3a0a8030043a742bc806e27276d0c781cfbadf40
joseph <j.hernandez.92189@gmail.com>
2022-09-19 11:02:27 -0700

    added confrimation screens for different password types

--- Develop/script.js
+++ Develop/script.js
@@ -16,6 +16,13 @@ function writePassword() {
 function generatePassword() {
   var password = '';
   var passwordLength = Number(prompt('How many characters would you like your password to be?'));
+
+  console.log(passwordLength);
+
+  var uppercase = confirm('Include uppercase letters in your password?');
+  var lowercase = confirm('Include lowercase letters in your password?');
+  var numbers = confirm('Include numbers in your password?');
+  var symbols = confirm('Include symbols in your password?');
 }
 
 // Add event listener to generate button
